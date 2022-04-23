fa2f09bcce4beb6fd32d238b1f3a89339908f32f
DESKTOP-V0ADQU6\user <m1006958@o365.fcu.edu.tw>
2022-04-23 17:04:19 +0800

    PDF 列表存取

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PdfViewPage from './pages/PdfViewPage';
 import PhotoPage from './pages/PhotoPage';
 import VideoPage from './pages/VideoPage'
 import Homepage from './pages/HomePage';
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdTrlKUDL7YRGKmubX7hLrPtNOeqHowfo",
+  authDomain: "gage-web.firebaseapp.com",
+  projectId: "gage-web",
+  storageBucket: "gage-web.appspot.com",
+  messagingSenderId: "535587474467",
+  appId: "1:535587474467:web:a0709d7388e9f5edea3596",
+  measurementId: "G-ER6N84GQDB"
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
 
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element = {<Homepage/>}/>
-      <Route path='/pdf_view' element={<PdfViewPage/>}/>
+      <Route path='/pdf_view' element={<PdfViewPage storage = {storage}/>}/>
       <Route path='/photo' element = {<PhotoPage/>}/>
       <Route path='/video' element = {<VideoPage/>}/>
     </Routes>
--- src/pages/PdfViewPage.js
+++ src/pages/PdfViewPage.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import { listAll,ref } from 'firebase/storage';
+import React from 'react'
 import Bar from '../components/Bar';
 import ViewerCom from '../components/ViewerCom';
 
-const PdfViewPage = () => {
+const PdfViewPage = ({storage}) => {
+    
+    const listRef = ref(storage, '/PDF')
+
+    listAll(listRef).then((res) =>{
+        res.items.forEach((item) =>{
+            console.log(item.name)
+        })
+    })
+
     return (
         <>
             <Bar/>
