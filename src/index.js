0dc6e359d9d2d9c09ca7f792c6823683e5ce82df
DESKTOP-V0ADQU6\user <m1006958@o365.fcu.edu.tw>
2022-04-23 17:09:03 +0800

    delete something

--- src/index.js
+++ src/index.js
@@ -13,13 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdTrlKUDL7YRGKmubX7hLrPtNOeqHowfo",
-  authDomain: "gage-web.firebaseapp.com",
-  projectId: "gage-web",
-  storageBucket: "gage-web.appspot.com",
-  messagingSenderId: "535587474467",
-  appId: "1:535587474467:web:a0709d7388e9f5edea3596",
-  measurementId: "G-ER6N84GQDB"
+
 };
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage(firebaseApp);
