b8b433f1bd5b1eb8dc1adb5772cd7e0bed447738
WenTee <superjimalex@gmail.com>
2022-06-08 20:33:27 +0800

    貼文樣式修改

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -1,8 +1,34 @@
+import { Col, Figure, Row } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { storage } from "../utils/firebaseAPI";
+import { ref, getDownloadURL } from "firebase/storage";
 import "./PhotoView.css";
 const Postview = ({ item }) => {
-  console.log(item)
+  const [imgURL, setimgURL] = useState(null);
+  const createDate = new Date(item.createTime.toMillis());
+  useEffect(() => {
+    const pathRef = ref(storage, 'Photo/'+ item.photoPath)
+    getDownloadURL(pathRef).then((url)=>{
+      setimgURL(url)
+    })
+  },[item.photoPath])
   return (
     <>
+      <Row className="post">
+        <Col sm={9}>
+          <Row className="postTitle">
+            <h2>{item.title}</h2>
+          </Row>
+          <Row className="postContent">
+            <p>{item.content}</p>
+          </Row>
+        </Col>
+        <Col sm={3}>
+          <Figure.Image width={300} height={300} src={imgURL}></Figure.Image>
+          <Figure.Caption>{`${createDate.getFullYear()}-${createDate.getDate()}-${createDate.getDay()} ${createDate.getHours()}:${createDate.getMinutes()}:${createDate.getSeconds()} 上傳`}</Figure.Caption>
+        </Col>
+        
+      </Row>
     </>
   );
 };
--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,47 +1,40 @@
-// import React, { useEffect, useState } from "react";
-import React,{ useState, useEffect } from 'react'
-// import { getInstanceStorage } from "../utils/firebaseAPI";
+import React, { useState, useEffect } from 'react'
 import Bar from "../components/Bar";
 import { firestore } from '../utils/firebaseAPI';
 import { collection, getDocs } from 'firebase/firestore';
 import Postview from '../components/PostView';
-// import Photoview from "../components/PhotoView";
-// import { listAll, ref } from "firebase/storage";
-const  PostPage = () => {
-  // const storage = getInstanceStorage();
+import { Container } from 'react-bootstrap';
+
+const PostPage = () => {
+
   const [docs, setDocs] = useState([]);
 
-  // function loadPhotoList() {
-  //   const listPhotoRef = ref(storage, "/Photo");
-  //   listAll(listPhotoRef).then((res) => {
-  //     setListFile(res.items);
-  //   });
-  // }
-  function getPosts(){
-    getDocs(collection(firestore, 'post')).then((querySnapshot) =>{
-      querySnapshot.forEach((doc)=>{
+  function getPosts() {
+    getDocs(collection(firestore, 'post')).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
         const data = {
-          id:doc.id,
-          item:doc.data()
+          id: doc.id,
+          item: doc.data()
         }
-        setDocs((prev) =>{
+        setDocs((prev) => {
           return [...prev, data]
         })
       })
     })
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getPosts()
   }, [])
   return (
     <>
       <Bar />
-
-      {docs.map((item) => {
-        return <Postview item={item.item} key={item.id} />
-      }
-      )}
+      <Container>
+        {docs.map((item) => {
+          return <Postview item={item.item} key={item.id} />
+        })}
+      </Container>
+      
     </>
   );
 };
