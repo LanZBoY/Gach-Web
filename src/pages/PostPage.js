9903256e77f2098f80c176841e122ca1ecd04c59
DESKTOP-V0ADQU6\user <m1006958@o365.fcu.edu.tw>
2022-06-22 15:54:34 +0800

    1.格式化文件 2.新增貼文上傳圖片預覽

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,58 +1,75 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import Bar from "../components/Bar";
-import { firestore } from '../utils/firebaseAPI';
-import { collection, getDocs } from 'firebase/firestore';
-import Postview from '../components/PostView';
-import { Button, Container, Form, Modal } from 'react-bootstrap';
+import { firestore } from "../utils/firebaseAPI";
+import { collection, getDocs } from "firebase/firestore";
+import Postview from "../components/PostView";
+import { Button, Container, Form, Modal, Figure } from "react-bootstrap";
 
 const PostPage = () => {
   const [docs, setDocs] = useState([]);
-  const [uploadDocs, setUploadDocs] = useState({
-  });
+  const [uploadDocs, setUploadDocs] = useState({});
+  const [hidePreviewImg, setHidePreviewImg] = useState(true);
+  const [previewImgURL, setPreviewImgURL] = useState(null);
   const [showAddModal, setShowAddModal] = useState(false);
-  const openAddModal = () =>{
-    setShowAddModal(true)
-  }
+  const openAddModal = () => {
+    setPreviewImgURL(null);
+    setUploadDocs({});
+    setHidePreviewImg(true);
+    setShowAddModal(true);
+  };
   const closeAddModal = () => {
-    setShowAddModal(false)
-  }
+    setShowAddModal(false);
+  };
 
   function getPosts() {
-    getDocs(collection(firestore, 'post')).then((querySnapshot) => {
+    getDocs(collection(firestore, "post")).then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         const data = {
           id: doc.id,
-          item: doc.data()
-        }
+          item: doc.data(),
+        };
         setDocs((prev) => {
-          return [...prev, data]
-        })
-      })
-    })
+          return [...prev, data];
+        });
+      });
+    });
   }
 
-  function handleUploadDocChange(event){
-    if(event.target.id === 'UploadTitle'){
+  function handleUploadDocChange(event) {
+    if (event.target.id === "UploadTitle") {
       setUploadDocs((prev) => {
-        prev.title = event.target.value
-        return prev
-      })
-    }else if(event.target.id === 'UploadContent'){
+        prev.title = event.target.value;
+        return prev;
+      });
+    } else if (event.target.id === "UploadContent") {
       setUploadDocs((prev) => {
-        prev.content = event.target.value
-        return prev
-      })
+        prev.content = event.target.value;
+        return prev;
+      });
+    } else if (event.target.id === "UploadFile") {
+      const file = event.target.files[0];
+      if (file !== undefined) {
+        setUploadDocs((prev) => {
+          prev.file = file;
+          return prev;
+        });
+        const fileReader = new FileReader();
+        fileReader.onload = function () {
+          setPreviewImgURL(this.result);
+        };
+        fileReader.readAsDataURL(file);
+        setHidePreviewImg(false);
+      }
     }
-
   }
 
-  function uploadDoc(){
-    console.log(uploadDocs)
+  function uploadDoc() {
+    console.log(uploadDocs);
   }
 
   useEffect(() => {
-    getPosts()
-  }, [])
+    getPosts();
+  }, []);
 
   return (
     <>
@@ -60,7 +77,7 @@ const PostPage = () => {
       <Button onClick={openAddModal}>新增</Button>
       <Container>
         {docs.map((item) => {
-          return <Postview item={item.item} key={item.id} />
+          return <Postview item={item.item} key={item.id} />;
         })}
       </Container>
       {/* 上傳貼文 */}
@@ -71,10 +88,33 @@ const PostPage = () => {
         <Modal.Body>
           <Form>
             <Form.Label>標題</Form.Label>
-            <Form.Control id='UploadTitle' type='text' onChange={handleUploadDocChange}></Form.Control>
+            <Form.Control
+              id="UploadTitle"
+              type="text"
+              onChange={handleUploadDocChange}
+            ></Form.Control>
             <Form.Label>內容</Form.Label>
-            <Form.Control as="textarea" id='UploadContent' type='text' onChange={handleUploadDocChange}></Form.Control>
+            <Form.Control
+              as="textarea"
+              id="UploadContent"
+              type="text"
+              onChange={handleUploadDocChange}
+            ></Form.Control>
+            <Form.Label>上傳圖片</Form.Label>
+            <Form.Control
+              id="UploadFile"
+              type="file"
+              size="lg"
+              onChange={handleUploadDocChange}
+            ></Form.Control>
           </Form>
+          <Figure.Image
+            id="PreviewImg"
+            hidden={hidePreviewImg}
+            width={300}
+            height={300}
+            src={previewImgURL}
+          ></Figure.Image>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={closeAddModal}>取消</Button>
--- src/utils/firebaseAPI.js
+++ src/utils/firebaseAPI.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyAdTrlKUDL7YRGKmubX7hLrPtNOeqHowfo",
   authDomain: "gage-web.firebaseapp.com",
